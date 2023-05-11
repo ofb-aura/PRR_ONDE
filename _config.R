cf2c79f6a698800e4b4142fae4d67ebe3ba33d36
ofb-aura <133232132+ofb-aura@users.noreply.github.com>
2023-05-11 16:19:43 +0200

    Adaptation du paramétrage à la région AURA

--- _config.R
+++ _config.R
@@ -1,4 +1,4 @@
-## configuration
+## configuration 
 conf_titre <- "[Prototype] Suivi des étiages (ONDE) en Auvergne Rhône-Alpes"
 conf_auteur <- "OFB - Direction régionale Auvergne Rhône-Alpes"
 conf_dep <- c("01", "03", "07", "15", "26", "38", "42", "43", "63", "69", "73", "74" )
